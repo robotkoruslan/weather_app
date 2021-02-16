12620e76b875e065e97aa4c83154758fcc27e964
Ruslan Robotko <robotko.ruslan@gmail.com>
2021-02-16 23:08:08 +0200

    Add API and search

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(WeatherApp());
 
@@ -9,7 +12,39 @@ class WeatherApp extends StatefulWidget {
 
 class _WeatherAppState extends State<WeatherApp> {
   int temperature = 0;
-  String location = 'Chicago';
+  int woeid = 922137;
+  String location = 'Kharkiv';
+  String weather = 'clear';
+  String searchApiUrl =
+      'https://www.metaweather.com/api/location/search/?query=';
+  String locationApiUrl = 'https://www.metaweather.com/api/location/';
+
+  void fetchSearch(String input) async {
+    var searchResult = await http.get(searchApiUrl + input);
+    var result = json.decode(searchResult.body)[0];
+
+    setState(() {
+      location = result['title'];
+      woeid = result['woeid'];
+    });
+  }
+
+  void fetchLocation() async {
+    var locationResult = await http.get(locationApiUrl + woeid.toString());
+    var result = json.decode(locationResult.body);
+    var consolidatedWeather = result['consolidated_weather'];
+    var data = consolidatedWeather[0];
+
+    setState(() {
+      temperature = data['the_temp'].round();
+      weather = data['weather_state_name'].replaceAll(' ', '').toLowerCase();
+    });
+  }
+
+  void onTextFieldSubmitted(String input) {
+    fetchSearch(input);
+    fetchLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,8 +81,11 @@ class _WeatherAppState extends State<WeatherApp> {
               Column(
                 children: <Widget>[
                   Container(
-                    width: 300,
+                    width: 250,
                     child: TextField(
+                      onSubmitted: (String input) {
+                        onTextFieldSubmitted(input);
+                      },
                       style: TextStyle(color: Colors.white, fontSize: 25),
                       decoration: InputDecoration(
                         hintText: 'Search location...',
